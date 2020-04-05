0233ba7b80c65abfa52bfeee5f4ee581450944fa
Vanya <rnnchik@gmail.com>
2020-04-05 13:52:28 +0200

    first comit

---
+++ script.js
@@ -0,0 +1,13 @@
+// 1
+// alert("Hello world");     модалка в браузере
+
+// 2
+// let answer = confirm("Are you here?"); модалка с 2 вариантпми ответа
+// console.log(answer); получаем булевое значение;
+
+// 3
+// let answer = prompt("Есть ли Вам 18?", "Да"); модалка с полем ввода для ответа
+// console.log(answer); получаем введенные данные в виде строки
+
+
+console.log('куку');
